b9281c31f05917cb7c9ade4fd2237db318a196f3
Tiaa28 <tniandari@gmail.com>
2023-02-27 20:18:30 +0700

    commit pertama

---
+++ main.js
@@ -0,0 +1,24 @@
+const navbar = document.getElementById("navbar");
+const text = document.getElementById("text");
+window.onscroll = () => {
+
+  if (window.scrollY >= 250) {
+    navbar.classList.add("colored");
+  } else {
+    navbar.classList.remove("colored");
+  }
+
+  if (window.scrollY > 50) {
+    text.style.transform = "scale(0)";
+  } else {
+    text.style.transform = "scale(1)";
+  }
+}
+
+function pesan() {
+  Swal.fire({
+    title: "Kepo ya?",
+    confirmButtonText: "Iya",
+    customClass: "swal-wide"
+  })
+}
